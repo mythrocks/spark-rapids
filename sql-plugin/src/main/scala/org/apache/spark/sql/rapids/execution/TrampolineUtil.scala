dd19460ab05a073b3550bc3b3b8370f8c8a9ddec
Raza Jafri <rjafri@nvidia.com>
2024-04-19 16:12:04 -0700

    AnalysisException child class
    Signed-off-by: Raza Jafri <rjafri@nvidia.com>

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/TrampolineUtil.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/execution/TrampolineUtil.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -154,7 +154,9 @@ object TrampolineUtil {
   }
 
   /** Throw a Spark analysis exception */
-  def throwAnalysisException(msg: String) = throw new AnalysisException(msg)
+  def throwAnalysisException(msg: String) = {
+    throw new AnalysisException(msg) {}
+  }
 
   /** Set the task context for the current thread */
   def setTaskContext(tc: TaskContext): Unit = TaskContext.setTaskContext(tc)
